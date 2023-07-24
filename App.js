88623f9f91b448fe6fc1f12e52fe313eef7eaea4
Jyotsana0408 <118711814+Jyotsana0408@users.noreply.github.com>
2023-07-24 20:02:46 +0530

    Add files via upload
    adding a form in expense tracker

---
+++ App.js
@@ -0,0 +1,53 @@
+//import logo from './logo.svg';
+import React from "react";
+//import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses.js";
+import NewExpense from "./components/NewExpense/NewExpense.js";
+
+import "./App.css";
+
+const App = () => {
+  const expenses = [
+    {
+      title: "Car Insurence",
+      Amount: 50000,
+      date: new Date(2023, 7, 15),
+      LocationOfExpenditure: "CarAgency",
+    },
+    {
+      title: "Toilet Paper",
+      Amount: 500,
+      date: new Date(2023, 6, 5),
+      LocationOfExpenditure: "Mall",
+    },
+    {
+      title: "Shoping",
+      Amount: 7000,
+      date: new Date(2023, 2, 10),
+      LocationOfExpenditure: "Clothes",
+    },
+    {
+      title: "Movie",
+      Amount: 1000,
+      date: new Date(2023, 5, 13),
+      LocationOfExpenditure: "Movie",
+    },
+  ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, {item: expenses })
+  // );
+
+    return (
+      <>
+        <div>
+          <NewExpense/>
+          <Expenses item={expenses}/>
+        </div>
+      </>
+  );
+}
+export default App;
