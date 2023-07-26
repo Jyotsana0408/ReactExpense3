33d96ca0e74ae103c2df47c08c6fb8e4ad74d6b0
Jyotsana0408 <118711814+Jyotsana0408@users.noreply.github.com>
2023-07-26 12:01:07 +0530

    Add files via upload
    filtered by year

--- Expenses.js
+++ Expenses.js
@@ -5,10 +5,15 @@ import ExpenseFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
+  
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.item.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <Card className="expenses">
@@ -16,7 +21,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.item.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key= {expense.id}
           title={expense.title}
