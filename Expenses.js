39c1f87883b9d6bc5d55e56bb8ce64830d657e38
Jyotsana0408 <118711814+Jyotsana0408@users.noreply.github.com>
2023-07-26 11:28:25 +0530

    Add files via upload
    added the keys

--- Expenses.js
+++ Expenses.js
@@ -1,45 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
-import Card from '../UI/Card.js';
+import Card from "../UI/Card.js";
 import ExpenseFilter from "./ExpensesFilter";
-import './Expenses.css';
+import "./Expenses.css";
 
 const Expenses = (props) => {
-
-  const [filteredYear , setFilteredYear] = useState('2020');
-  const filterChangeHandler = selectedYear => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
-  
-    return(
-   <Card className='expenses'>
-    <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}  />
-          <ExpenseItem
-          title={props.item[0].title}
-          Amount={props.item[0].Amount}
-          date={props.item[0].date}
-          LocationOfExpenditure={props.item[0].LocationOfExpenditure}
-        ></ExpenseItem>
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {props.item.map((expense) => (
         <ExpenseItem
-          title={props.item[1].title}
-          Amount={props.item[1].Amount}
-          date={props.item[1].date}
-          LocationOfExpenditure={props.item[1].LocationOfExpenditure}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.item[2].title}
-          Amount={props.item[2].Amount}
-          date={props.item[2].date}
-          LocationOfExpenditure={props.item[2].LocationOfExpenditure}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.item[3].title}
-          Amount={props.item[3].Amount}
-          date={props.item[3].date}
-          LocationOfExpenditure={props.item[3].LocationOfExpenditure}
-        ></ExpenseItem>
-   </Card>
-    );
-}
+          key= {expense.id}
+          title={expense.title}
+          Amount={expense.Amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+};
 
 export default Expenses;
