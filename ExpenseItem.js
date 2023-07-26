fa21a88f27d618c55b4f165839ad9ae05992a7ea
Jyotsana0408 <118711814+Jyotsana0408@users.noreply.github.com>
2023-07-26 10:05:56 +0530

    Add files via upload
    add filter

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,40 +1,17 @@
-import React, { useState} from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card.js';
 
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
-  // const title = props.title;
-  // const amount = props.amount;
-  // const LocationOfExpenditure = props.LocationOfExpenditure;
-  const [title, setTitle] = useState(props.title);
-  const [Amount, setAmount] = useState(props.Amount);
-
-
-  const clickHandler = () =>{
-    setTitle('Updated!!');
-    console.log(title);
-  }
-  const changeAmount = () =>{
-    setAmount('100$');
-    console.log(Amount);
-  }
-
   return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
           <div className='expense-item_description'>
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             </div>
-          <div className='LocationOfExpenditure mx-2'>
-            <h3>{props.LocationOfExpenditure}</h3>
-            </div>
-          <div className='expense-item_price'>${Amount} </div> 
-          <button onClick={clickHandler}>Change title</button>
-          <button onClick={changeAmount}>Change Amount</button>
-
+          <div className='expense-item_price'>${props.Amount} </div> 
     </Card>  
 )
   }
